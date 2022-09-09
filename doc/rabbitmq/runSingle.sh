b40b3c420ee02584fc2dcf019a15484500b2a584
cyylog <cyylog@163.com>
2022-09-09 15:54:01 +0800

    2022年09月 9日 15:54:01

--- doc/rabbitmq/runSingle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker rm -f rabbitmq
-docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 \
-        -v /export/rabbitmq/data:/var/lib/rabbitmq --hostname myRabbit \
-        -e RABBITMQ_DEFAULT_VHOST=/  \
-        -e RABBITMQ_DEFAULT_USER=admin \
-        -e RABBITMQ_DEFAULT_PASS=admin \
-        10.7.92.101:5000/rabbitmq:3.8.11-management-alpine
